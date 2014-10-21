8f368ed544672b744c1979b7afdd109816a878e9
Alastair Porter <alastair.porter@upf.edu>
2014-10-21 11:19:05 +0200

    per-track artists

--- jingju/management/commands/addjingju.py
+++ jingju/management/commands/addjingju.py
@@ -12,7 +12,7 @@ class Command(BaseCommand):
 
         last_title = ""
         last_number = 0
-        disc = 1
+        disc = 0
         therel = None
         for l in fp:
             print l
@@ -27,12 +27,14 @@ class Command(BaseCommand):
                 recdate = l["Recording date"]
                 barcode = l["Barcode"]
                 if title != last_title:
-                    disc = 1
+                    disc = 0
                     therel = models.Release.objects.create(title=title, artist=artist, label=label, catno=cat, reldate=reldate, recdate=recdate, barcode=barcode)
                     last_title = title
                 if number < last_number:
                     disc += 1
-                thetrack = models.Track.objects.create(release=therel, name=track_name, disc=disc, position=number)
+                if disc == 0:
+                    disc += 1
+                thetrack = models.Track.objects.create(release=therel, name=track_name, disc=disc, position=number, artists=artist)
                 last_number = number
 
         #self.stdout.write()
--- jingju/models.py
+++ jingju/models.py
@@ -1,3 +1,4 @@
+# encoding:utf-8
 from django.db import models
 
 class Release(models.Model):
@@ -16,9 +17,15 @@ class Release(models.Model):
 class Track(models.Model):
     release = models.ForeignKey(Release)
     name = models.CharField(max_length=200)
+    artists = models.CharField(max_length=200)
     position = models.IntegerField()
     disc = models.IntegerField()
 
     def __unicode__(self):
         return self.name
+
+    def artist_list(self):
+        # this is a chinese comma, not an ascii one
+        names = [a.strip() for a in self.artists.split(u"，")]
+        return enumerate(names)
 
